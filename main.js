0e1969eedb428809907a49551e7d9f7037879b9b
Sivak Hico <strukggg@gmail.com>
2022-09-14 15:24:34 +0200

    update ...

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-const input = document.querySelector('#input-txt');
+const toDo = ["walk", "wash", "watch", "drive", "sleep", "eat"];
 
-input.addEventListener('keyup', function(event){
-    
+const input = document.querySelector("#input-txt");
+const add = document.querySelector("#add");
 
-    console.log(input.event); 
+input.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    toDo.push(input.value);
+    document.querySelector("#content").innerHTML = toDo;
+    input.value = '';
+  }
 });
 
+add.addEventListener("click", (e) => {
+  document.querySelector("#content").innerText = toDo;
+  input.value = '';
+});
 
-
-console.log('55');  
+console.log(toDo);
